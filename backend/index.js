b411eabfacc09d385685a90332ae01ef63966df4
Tushar jain <tusharjain25tj2001@gmail.com>
2022-07-08 12:32:24 +0530

    routes added

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ const express = require('express')
 connectToMongo();
 const app = express()
 const port = 3000
-app.get('/', (req, res) => {
-    res.send('Hello tushar')
-})
+
+app.use(express.json())
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
