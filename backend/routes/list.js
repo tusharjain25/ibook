f6720215c4bd643229715820f7ff98ea6f7da7d1
Tushar jain <tusharjain25tj2001@gmail.com>
2022-07-12 00:28:11 +0530

    Revert "database connected"
    This reverts commit f180de7678871ffc66c8b18fbb5af5696f82e425.

---
+++ backend/routes/list.js
@@ -0,0 +1,44 @@
+const express= require('express');
+const router=express.Router();
+var fetchuser=require('../middleware/fetchuser');  
+const list=require('../models/List')
+const { body, validationResult } = require("express-validator");
+
+
+router.get('/fetchallnotes',fetchuser,async (req,res)=>{
+    try {
+        const notes=await Note.find({user:req.user.id});
+    res.json(notes)
+    } catch (error) {
+        console.error(error.message);
+      res.status(500).send("internal server error");
+    }
+    
+})
+
+
+router.post('/addnote',fetchuser, [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "description must be of atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],async (req,res)=>{
+    try {
+        const{title,description,tag}=req.body;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        const note=new Note({
+            title,description,tag,user:req.user.id
+        })
+        const savedNote=await note.save()
+        res.json(savedNote)
+    } catch (error) {
+        console.error(error.message);
+      res.status(500).send("internal server error");
+    }
+  
+})
+
+module.exports=router
