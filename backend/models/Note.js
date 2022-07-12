f848cf6fea8a1e2b8905136b984d41da3d048d75
Tushar jain <tusharjain25tj2001@gmail.com>
2022-07-12 07:35:26 +0530

    updation added

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -5,9 +5,17 @@ const NoteSchema = new Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:'user'
     },
-    Title: {
+    title: {
         type: String,
         required: true
+    }, 
+     description: {
+        type: String,
+        required: true,
+    },
+    tag: {
+        type: String,
+        default: "General"
     },
     date: {
         type: Date,
